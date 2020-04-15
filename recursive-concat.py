b571d8bc0b504d3016bb98bc506c474cba72679a
John Gabriele <jgabriele@fastmail.fm>
2020-04-15 15:13:01 -0400

    Create recursive-concat.py

---
+++ recursive-concat.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+# Prints out contents of all txt or md files in this directory and below.
+#
+# Usage:
+#
+#     recursive-concat.py > out
+#
+# It goes in shell/asciibetical order, so if you want things in a certain
+# order, name them like 010_this.txt, 011_that_dir, 012_other.txt, etc.
+# (nested deep as you like).
+
+import os, os.path, io
+
+all_fnms = []
+for (the_dir, dirs, fnms) in os.walk("."):
+    for fnm in fnms:
+        if fnm.endswith(".txt") or fnm.endswith(".md"):
+            all_fnms.append(os.path.join(the_dir, fnm))
+
+all_fnms.sort()
+
+for fnm in all_fnms:
+    print(io.open(fnm).read(), "\n")
